d3c6f6c7385549dcf9e8f86923c5d10e34628126
Aleksei Savelev <savelichalex93@gmail.com>
2023-07-18 22:51:21 +0800

    Initial infrustructure setup for Snowplow

---
+++ analytics/infrustructure/snowplow/variables.tf
@@ -0,0 +1,31 @@
+variable "prefix" {
+  description = "Will be prefixed to all resource names. Use to easily identify the resources created"
+  type        = string
+}
+
+variable "project_id" {
+  description = "The project ID in which the stack is being deployed"
+  type        = string
+}
+
+variable "region" {
+  description = "The name of the region to deploy within"
+  type        = string
+}
+
+variable "bigquery_loader_dead_letter_bucket_deploy" {
+  description = "Whether this module should create a new bucket with the specified name - if the bucket already exists set this to false"
+  default     = true
+  type        = bool
+}
+
+variable "vendor" {
+  description = "An unique identifier like 'com.snowplow.analytics for the vendor of this stack."
+  type        = string
+}
+
+variable "labels" {
+  description = "The labels to append to the resources in this module"
+  default     = {}
+  type        = map(string)
+}
