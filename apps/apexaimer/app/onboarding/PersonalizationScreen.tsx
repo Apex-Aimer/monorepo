e4f7695cc0c0ae9fce3b533babc292412c8ecd32
Aleksei Savelev <savelichalex93@gmail.com>
2024-02-23 19:09:39 +0800

    Use difficulty level at onboarding personalization screen

--- apps/apexaimer/app/onboarding/PersonalizationScreen.tsx
+++ apps/apexaimer/app/onboarding/PersonalizationScreen.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { View, StyleSheet, Text, Image } from 'react-native'
 import { useHeaderHeight } from '@react-navigation/elements'
 import Animated, {
@@ -10,14 +10,19 @@ import Animated, {
 } from 'react-native-reanimated'
 import { LinearGradient } from 'expo-linear-gradient'
 import { ArrowRightIcon } from 'react-native-heroicons/solid'
-import Rive, { Fit, LoopMode, RiveRef } from 'rive-react-native'
+import Rive, { Fit, RiveRef } from 'rive-react-native'
+import { useRecoilValue } from 'recoil'
 
 import { AppStyleSheet, useAppStyles } from '../components/useAppStyles'
 import { useThemeColors } from '../components/ThemeProvider'
 import { OnboardingScreenCTA } from './components/OnboardingScreenCTA'
 import { useOnboardingFadeOut } from './components/OnboardingFadeInOutView'
+import { level as levelAtom } from '../store'
 // @ts-ignore
 import GeneratingPersonalizationAnimation from '../../assets/generatingpersonalization.riv'
+// @ts-ignore
+import RevealAnimation from '../../assets/reveal.riv'
+import { DifficultyLevelIcon } from '../components/DifficultyLevelIcon'
 
 interface LoaderProgressProps {
   onEnd(): void
@@ -30,7 +35,6 @@ function LoaderProgress({ onEnd }: LoaderProgressProps) {
   const width = useSharedValue(0)
 
   useEffect(() => {
-    Easing.ease
     width.value = withTiming(
       StyleSheet.flatten(styles.rail).width as number,
       {
@@ -64,44 +68,65 @@ export function PersonalizationScreen() {
   const headerHeight = useHeaderHeight()
   const fadeOut = useOnboardingFadeOut()
   const [isLoaded, setLoaded] = useState(false)
+  const level = useRecoilValue(levelAtom)
 
-  const onEnd = useCallback(() => {
-    setLoaded(true)
-  }, [])
+  const loadingRiveRef = useRef<RiveRef>(null)
+  const revealRiveRef = useRef<RiveRef>(null)
 
-  const riveRef = useRef<RiveRef>(null)
+  const loadingOpacity = useSharedValue(1)
+  const badgeOpacity = useSharedValue(0)
+  const badgeScale = useSharedValue(0.2)
+
+  const loadingStyle = useAnimatedStyle(() => ({
+    opacity: loadingOpacity.value,
+  }))
+
+  const badgeStyle = useAnimatedStyle(() => ({
+    opacity: badgeOpacity.value,
+    transform: [
+      {
+        scale: badgeScale.value,
+      },
+    ],
+  }))
 
   return (
     <View style={[styles.container, { paddingTop: headerHeight }]}>
       <View style={styles.inner}>
         <View style={styles.loadingAnimation}>
-          <Rive
-            ref={riveRef}
-            url={
-              Image.resolveAssetSource(GeneratingPersonalizationAnimation).uri
-            }
-            artboardName="banner"
-            style={styles.loadingAnimation}
-            autoplay={true}
-            onPause={() => {
-              riveRef.current?.play()
-            }}
-            fit={Fit.Cover}
-            // onPause={() => {
-            //   bannerPlacement.value = withDelay(
-            //     200,
-            //     withTiming(
-            //       1,
-            //       {
-            //         duration: 500,
-            //       },
-            //       () => {
-            //         runOnJS(onEnd)()
-            //       }
-            //     )
-            //   )
-            // }}
-          />
+          <Animated.View style={[styles.loadingAnimation, loadingStyle]}>
+            <Rive
+              ref={loadingRiveRef}
+              url={
+                Image.resolveAssetSource(GeneratingPersonalizationAnimation).uri
+              }
+              artboardName="banner"
+              style={styles.loadingAnimation}
+              autoplay={true}
+              onPause={() => {
+                loadingRiveRef.current?.play()
+              }}
+              fit={Fit.Cover}
+            />
+          </Animated.View>
+          <View style={styles.revealAnimationWrapper}>
+            <Rive
+              ref={revealRiveRef}
+              url={Image.resolveAssetSource(RevealAnimation).uri}
+              artboardName="banner"
+              fit={Fit.Cover}
+              autoplay={false}
+              onPause={() => {
+                setLoaded(true)
+              }}
+              style={styles.revealAnimation}
+            />
+          </View>
+          <View style={styles.badgeWrapper}>
+            <Animated.View style={badgeStyle}>
+              <DifficultyLevelIcon size={120} level={level} />
+            </Animated.View>
+          </View>
         </View>
         {isLoaded ? (
           <Text style={styles.description}>
@@ -111,7 +136,15 @@ export function PersonalizationScreen() {
           </Text>
         ) : (
           <View style={styles.loadingContainer}>
-            <LoaderProgress onEnd={onEnd} />
+            <LoaderProgress
+              onEnd={() => {
+                badgeOpacity.value = withTiming(1, { duration: 300 })
+                badgeScale.value = withTiming(1, { duration: 300 }, () => {
+                  loadingOpacity.value = withTiming(0)
+                })
+                revealRiveRef.current?.play()
+              }}
+            />
             <Text style={styles.description}>
               We’re identifying a difficulty level for you...
             </Text>
@@ -134,6 +167,7 @@ const themedStyles = AppStyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'bg',
+    paddingBottom: 100,
   },
   inner: {
     flex: 1,
@@ -169,5 +203,26 @@ const themedStyles = AppStyleSheet.create({
   loadingAnimation: {
     width: '100%',
     aspectRatio: 2,
+  },
+  revealAnimationWrapper: {
+    position: 'absolute',
+    top: -80,
+    bottom: -80,
+    left: 0,
+    right: 0,
+    flex: 1,
+    alignItems: 'center',
+  },
+  revealAnimation: {
+    aspectRatio: 1,
+  },
+  badgeWrapper: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 })
